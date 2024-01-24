2a6cf2891b8bfe149fa4060103692c2ccee9782c
Andrew Wang <44416801+aywang71@users.noreply.github.com>
2024-01-24 10:24:21 -0500

    hw1

---
+++ HW1/homework1.py
@@ -0,0 +1,58 @@
+"""
+DO NOT MODIFY ANY VARIABLE NAMES. YOU MAY COMMENT YOUR CODE IF NEEDED.
+"""
+
+# You must fill in your Pennkey and your 8 digit Penn ID below:
+
+pennkey = ""
+penn_id = ""
+
+
+# ORACLE QUERIES
+
+answer1 = """ """
+
+answer2 = """ """
+
+answer3 = """ """
+
+answer4 = """ """
+
+answer5 = """ """
+
+answer6 = """ """
+
+answer7 = """ """
+
+
+# MYSQL QUERIES
+
+# The database you set up on RDS - SPECIFICALLY FOR SUBMISSION
+
+db_config = {
+    "username" : "",
+    "host": "",
+    "port": "",
+    "password": ""
+}
+
+
+# Fill in DDL statements in the order of execution. A deduction might be applied for an incorrect order.
+# Create query for ___
+answer8a = """ """
+# Create query for ___
+answer8b = """ """
+# Create query for ___
+answer8c = """ """
+
+
+answer9 = """ """
+
+answer10 = """ """
+
+answer11 = """ """
+
+answer12 = """ """
+
+# Place your text response to answer 13 within the multi-line string below.
+answer13 = """ """

5d06ce42b4e35ce40a0f2d1bfe3daa325f8c2d5e
Andrew Wang <44416801+aywang71@users.noreply.github.com>
2024-02-11 17:05:26 -0500

    hw1 update

--- HW1/homework1.py
+++ HW1/homework1.py
@@ -10,7 +10,7 @@ penn_id = "87457877"
 
 # ORACLE QUERIES
 
-answer1 = """SELECT NAME
+answer1 = """SELECT DISTINCT NAME
 FROM MOVIE_CAST
 WHERE NAME LIKE 'S%'
 AND SUBSTR(NAME, -1, 1) IN ('a', 'e', 'i', 'o', 'u')
@@ -98,20 +98,25 @@ JOIN MOVIE
 GROUP BY cd.GENRE_NAME"""
 
 answer7 = """WITH dir AS (
-    SELECT CREW_ID, COUNT(*) AS num, AVG(MOVIE.RATING) AS avg_rating,
+    SELECT CREW_ID,
            CASE
-                WHEN COUNT(*) > 5 THEN 'Experienced'
-                WHEN COUNT(*) <= 5 THEN 'Novice'
+                WHEN COUNT(*) >= 5 THEN 'Experienced'
+                WHEN COUNT(*) < 5 THEN 'Novice'
             END AS exp_level
     FROM CREW_IN
-    JOIN MOVIE
-        ON CREW_IN.MOVIE_ID = MOVIE.MOVIE_ID
     WHERE job = 'Director'
     GROUP BY CREW_ID
-    ORDER BY CREW_ID ASC
+), mov AS (
+    SELECT movie.RATING, dir.exp_level
+    FROM CREW_IN
+    JOIN MOVIE
+        ON movie.MOVIE_ID = crew_in.MOVIE_ID
+    JOIN dir
+        ON dir.CREW_ID = CREW_IN.CREW_ID
+    WHERE CREW_IN.JOB = 'Director'
 )
-SELECT exp_level, ROUND(AVG(avg_rating), 2) AS avg_rating
-FROM dir
+SELECT exp_level, ROUND(AVG(RATING), 2) AS avg_rating
+FROM mov
 GROUP BY exp_level
 ORDER BY avg_rating DESC"""
 
@@ -180,7 +185,7 @@ answer10 = """WITH ir AS(
     FROM ir
     JOIN Airports
         ON ir.target_id = Airports.id
-    WHERE source_country != country
+    WHERE source_country <> country
     GROUP BY airline_id
 )
 SELECT name AS airline_name, num_cities
@@ -188,30 +193,88 @@ FROM ir2
 JOIN Airlines
     ON ir2.airline_id = Airlines.id
 WHERE num_cities > 150
-ORDER BY num_cities, name"""
+ORDER BY num_cities DESC, name"""
 
 answer11 = """WITH mx AS (
     SELECT id, city
     FROM Airports
     WHERE country = 'Mexico'
 ), num AS (
-    SELECT COUNT(*) AS n
+    SELECT COUNT(DISTINCT city) AS n
     FROM mx
-), ar AS (
-    SELECT id, city, target_id
+), mxRoutes AS (
+    SELECT *
+    FROM Routes
+    JOIN mx
+        ON mx.id = Routes.target_id
+) , ar AS (
+    SELECT mx.id, mx.city, target_id
     FROM mx
-    JOIN Routes
-        ON mx.id = Routes.source_id
+    JOIN mxRoutes
+        ON mx.id = mxRoutes.source_id
 ), fin AS (
     SELECT city, num.n - 1 - COUNT(DISTINCT target_id) AS num_cities
     FROM ar, num
     GROUP BY city
+), noRoutes AS (
+    SELECT DISTINCT city, 86 AS num_cities
+    FROM mx
+), res AS (
+    SELECT * FROM fin
+    UNION
+    SELECT * FROM noRoutes
 )
-SELECT *
-FROM fin
+SELECT city, MIN(num_cities) AS num_cities
+FROM res
+GROUP BY city
 ORDER BY num_cities ASC, city"""
 
-answer12 = """ """
+answer12 = """WITH u1 AS ( #find all UK airlines
+    SELECT *
+    FROM Airlines
+    WHERE country = 'United Kingdom'
+), UKRoutes AS ( #find all routes flown by UK airlines
+    SELECT *
+    FROM Routes
+    JOIN u1
+        ON Routes.airline_id = u1.id
+), wa AS ( #find all airports in Washington
+    SELECT id, city
+    FROM Airports
+    WHERE city = 'Washington'
+), w1 AS ( #find all cities 1 flight from Washington
+    SELECT DISTINCT target_id AS t1, Airports.city AS city, 1 AS paid_flights
+    FROM UKRoutes
+    JOIN Airports
+        ON Airports.iata = UKRoutes.target_iata_icao
+    WHERE source_id IN (SELECT id FROM Airports WHERE city = 'Washington')
+), w2 AS ( #find all cities 2 flight from Washington
+    SELECT DISTINCT target_id AS t2, Airports.city AS city, 2 AS paid_flights
+    FROM UKRoutes
+    JOIN w1
+        ON UKRoutes.source_id = w1.t1
+    JOIN Airports
+        ON Airports.iata = UKRoutes.target_iata_icao
+), nj1 AS ( #find all UKRoutes start at newark
+    SELECT DISTINCT target_id AS n1, Airports.city AS city, 1 AS paid_flights
+    FROM UKRoutes
+    JOIN Airports
+        ON Airports.iata = UKRoutes.target_iata_icao
+    WHERE src_iata_icao = 'EWR'
+),fin AS (
+    SELECT * #need to remove duplicates and take the minimums for output
+    FROM w1
+    UNION
+    SELECT *
+    FROM w2
+    UNION
+    SELECT *
+    FROM nj1
+)
+SELECT city, MIN(paid_flights) AS paid_flights
+FROM fin
+WHERE city <> 'Washington' AND city <> 'Newark'
+GROUP BY city"""
 
 # Place your text response to answer 13 within the multi-line string below.
 answer13 = """I did not use generative AI to assist me in the homework. I used GeeksForGeeks documentation and syntax pages to help me understand SQL statements though"""

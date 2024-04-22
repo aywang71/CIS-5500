ca5ec777f37e08e05e8ca0ce70ddd9c7f7f27ed5
Andrew Wang <44416801+aywang71@users.noreply.github.com>
2024-04-22 14:06:50 -0400

    hw6 init

---
+++ HW6/B6plhcGtzveqTstt2RWw1awb.js
@@ -0,0 +1,20 @@
+// Question 7
+var answer_7 = ``
+
+// Question 8
+var answer_8 = ``
+
+// Question 9
+var answer_9 = ``
+
+// Question 10
+var answer_10 = ``
+
+// Question 11
+var answer_11 = ``
+
+// Question 12
+var answer_12 = ``
+
+// DO NOT MODIFY BELOW THIS LINE
+module.exports =  { answer_7, answer_8, answer_9, answer_10, answer_11, answer_12 }
---
+++ HW6/vqqIP3bD2bGVPZahtdjd8GHw.js
@@ -0,0 +1,17 @@
+// Question 1
+var answer_1 = null
+
+// Question 2
+var answer_2 = null
+
+// Question 3
+var answer_3 = null
+
+// Question 4
+var answer_4 = null
+
+// Question 5
+var answer_5 =  null
+
+// Question 6
+var answer_6 = null
